4c6c1b8aa19c46a512e22b24915bfd3199486fad
Jon Chapman <jochapjo@icloud.com>
2026-05-04 11:55:25 -0500

    checkpoint

--- R/types.R
+++ R/types.R
@@ -1,111 +1,3 @@
-rsgl_types <- function(class) {
-  all_types <- c(
-    "BIGINT",
-    "INT8", # BIGINT alias
-    "LONG", # BIGINT alias
-    "BOOLEAN",
-    "BOOL", # BOOLEAN alias
-    "LOGICAL", # BOOLEAN alias
-    "DATE",
-    "DECIMAL",
-    "TEST_ENUM",
-    "NUMERIC", # DECIMAL alias
-    "DOUBLE",
-    "FLOAT8", # DOUBLE alias
-    "FLOAT",
-    "FLOAT4", # FLOAT alias
-    "REAL", # FLOAT alias
-    "HUGEINT",
-    "INTEGER",
-    "INT4", # INTEGER alias
-    "INT", # INTEGER alias
-    "SIGNED", # INTEGER alias
-    "INTERVAL",
-    "SMALLINT",
-    "INT2", # SMALLINT alias
-    "SHORT", # SMALLINT alias
-    "TIME",
-    "TIMESTAMP WITH TIME ZONE",
-    "TIMESTAMPTZ", # TIMESTAMP WITH TIME ZONE alias
-    "TIMESTAMP",
-    "DATETIME", # TIMESTAMP alias
-    "TINYINT",
-    "INT1", # TINYINT alias
-    "UBIGINT",
-    "UHUGEINT",
-    "UINTEGER",
-    "USMALLINT",
-    "UTINYINT",
-    "UUID",
-    "VARCHAR",
-    "CHAR", # VARCHAR alias
-    "BPCHAR", # VARCHAR alias
-    "TEXT", # VARCHAR alias
-    "STRING" # VARCHAR alias
-  )
-  if (missing(class)) {
-    return(all_types)
-  }
-
-  con <- DBI::dbConnect(duckdb::duckdb())
-  enum_type_stmt <- create_enum_stmt_for_type_test()
-  DBI::dbExecute(con, enum_type_stmt)
-  tbl_stmt <- create_tbl_stmt_for_type_test(all_types)
-  DBI::dbExecute(con, tbl_stmt)
-  df <- DBI::dbGetQuery(con, "select * from type_test")
-  class_check_fn_list <- list(
-    "numerical" = is_numerical_col,
-    "categorical" = is_categorical_col,
-    "temporal" = is_temporal_col
-  )
-  class_check_fn <- class_check_fn_list[[class]]
-  types_in_class <- c()
-  for (col_name in colnames(df)) {
-    if (class_check_fn(df[[col_name]])) {
-      duckdb_type <- substring(col_name, 5)
-      types_in_class <- c(types_in_class, duckdb_type)
-    }
-  }
-  types_in_class
-}
-
-create_enum_stmt_for_type_test <- function() {
-  "create type TEST_ENUM as ENUM ('test')"
-}
-
-create_tbl_stmt_for_type_test <- function(types) {
-  column_defs <- paste0("\t\"COL_", types, "\" ", types, collapse = ",\n")
-  paste0("create table type_test (\n", column_defs, "\n)")
-}
-
-valid_duckdb_types <- function(types) {
-  con <- DBI::dbConnect(duckdb::duckdb())
-  enum_type_stmt <- create_enum_stmt_for_type_test()
-  DBI::dbExecute(con, enum_type_stmt)
-  tbl_stmt <- create_tbl_stmt_for_type_test(types)
-  DBI::dbExecute(con, tbl_stmt)
-}
-
-types_are_supported_by_duckdb <- function() {
-  valid_duckdb_types(rsgl_types())
-}
-
-map_rsgl_types_to_r_classes <- function() {
-  map <- list()
-  con <- DBI::dbConnect(duckdb::duckdb())
-  enum_type_stmt <- create_enum_stmt_for_type_test()
-  DBI::dbExecute(con, enum_type_stmt)
-  tbl_stmt <- create_tbl_stmt_for_type_test(rsgl_types())
-  DBI::dbExecute(con, tbl_stmt)
-  df <- DBI::dbGetQuery(con, "select * from type_test")
-  for (col_name in colnames(df)) {
-    duckdb_type <- substring(col_name, 5)
-    r_class <- class(df[[col_name]])[1]
-    map[[r_class]] <- c(map[[r_class]], duckdb_type)
-  }
-  map
-}
-
 is_numerical_col <- function(col) {
   numerical_classes <- c("numeric", "integer", "difftime")
   class(col)[1] %in% numerical_classes
@@ -141,7 +33,6 @@ is_categorical_mapping <- function(layer, df, aes) {
   }
 }
 
-
 is_temporal_mapping <- function(layer, df, aes) {
   aes_mapping <- layer$aes_mappings[[aes]]
   if (col_expr_has_cta(aes_mapping, "count")) {
@@ -159,4 +50,50 @@ is_binned_mapping <- function(layer, aes) {
   } else {
     FALSE
   }
+}
+
+#' Get SGL type classificatons for columns in a table
+#'
+#' `type_classifications` takes a DBI connection and a table name
+#' and returns the SGL type classifications (numerical, categorical,
+#' or temporal) of the table's columns.
+#'
+#' @param con A DBI connection
+#' @param table_name The name of a table
+#'
+#' @return A dataframe listing the SGL type classification of each column.
+#'
+#' @examples
+#' library(duckdb)
+#' con <- dbConnect(duckdb())
+#' dbWriteTable(con, "iris", iris)
+#' type_classifications(con, "iris")
+#'
+#' @export
+type_classifications <- function(con, table_name) {
+  sql <- sprintf(
+    "select * from %s limit 1",
+    table_name
+  )
+  df <- DBI::dbGetQuery(con, sql)
+
+  col_class <- function(col) {
+    if (is_numerical_col(col)) {
+      "numerical"
+    } else if (is_categorical_col(col)) {
+      "categorical"
+    } else if (is_temporal_col(col)) {
+      "temporal"
+    } else {
+      "unknown"
+    }
+  }
+  classes <- purrr::map_chr(
+    df, col_class
+  )
+
+  data.frame(
+    column_name = names(df),
+    column_class = unname(classes)
+  )
 }
